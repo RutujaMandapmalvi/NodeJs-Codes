685fcce2c37980ad8cb8b8948ef7b65b679e1d8c
rutuja <rutujamandapmalvi@gmail.com>
2024-02-21 22:32:24 +0530

    fs - read & write file(synchronous manner)

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-//Read data line by line
-const readLine = require('readline');
-const rl = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+//1. Read data line by line
+// const readLine = require('readline');
+// const rl = readLine.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-rl.question("what's your name? ", (name) => {
-    console.log('My name is '+name);
-    rl.close();
-})
+// rl.question("what's your name? ", (name) => {
+//     console.log('My name is '+name);
+//     rl.close();
+// })
 
-rl.on('close', () => {
-    // console.log('interface closed');
-    process.exit(0);
-})
+// rl.on('close', () => {
+//     // console.log('interface closed');
+//     process.exit(0);
+// })
+
+
+//2. fs - read & write file(synchronous manner):
+// const readline = require('readline');
+// const fs = require('fs');
+// let fileContent = fs.readFileSync('./input.txt', 'utf-8');
+// console.log(fileContent);
+
+// let content = `${fileContent} \n${new Date()} `
+// fs.writeFileSync('./writeHere.txt', content);
+// console.log(content)
