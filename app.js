8923ffd361d027be3b28463e812395f95277dd32
rutuja <rutujamandapmalvi@gmail.com>
2024-02-22 11:18:21 +0530

    fs - read & write file(asynchronous manner)

--- app.js
+++ app.js
@@ -23,5 +23,23 @@
 // console.log(fileContent);
 
 // let content = `${fileContent} \n${new Date()} `
-// fs.writeFileSync('./writeHere.txt', content);
+// fs.writeFileSync('./output.txt', content);
 // console.log(content)
+
+//3. fs - read & write file(asynchronous manner):
+// const fs = require('fs');
+// fs.readFile('./start.txt', 'utf-8', (error1, data1) => {
+//     console.log(data1);
+//     fs.readFile(`./${data1}.txt`, 'utf-8', (error2, data2) => {
+//         console.log(`${data1}.txt`);
+//         console.log(data2)
+//         console.log(error2);
+//         fs.readFile('./append.txt', 'utf-8', (error3, data3) => {
+//             console.log(data3)
+//             fs.writeFile('./output.txt', `${data2} \n\n${data3}`, () => {
+//                 console.log('Go check your file');
+//             })
+//         })
+//     })
+// })
+// console.log("I'm reading the file but i'm also moving on to the next action which depicts asynchronous nature.")
