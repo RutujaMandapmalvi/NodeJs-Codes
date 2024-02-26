9aad3aa62245b442b38376e42a70e048f737a319
rutuja <rutujamandapmalvi@gmail.com>
2024-02-26 13:53:33 +0530

    arranged folders and added contacts html code

---
+++ Modules/replaceHtml.js
@@ -0,0 +1,13 @@
+module.exports = function replaceHtml(template, product){
+    let output = template.replace("{{%NAME%}}", product.name);
+    output = output.replace("{{%MODELNAME%}}", product.modeName);
+    output = output.replace("{{%MODELNO%}}", product.modelNumber);
+    output = output.replace("{{%AGE%}}", product.age);
+    output = output.replace("{{%BEHAVIOUR%}}", product.behaviour);
+    output = output.replace("{{%DESCRIPTION%}}", product.Description);
+    output = output.replace("{{%PRICE%}}", product.price);
+    output = output.replace("{{%COLOR%}}", product.color);
+    output = output.replace("{{%PRODUCTIMAGE%}}", product.productImage);
+    output = output.replace("{{%ID%}}", product.id);
+    return output;
+}
--- app.js
+++ app.js
@@ -19,24 +19,24 @@
 //2. fs - read & write file(synchronous manner):
 // const readline = require('readline');
 // const fs = require('fs');
-// let fileContent = fs.readFileSync('./input.txt', 'utf-8');
+// let fileContent = fs.readFileSync('./Data/input.txt', 'utf-8');
 // console.log(fileContent);
 
 // let content = `${fileContent} \n${new Date()} `
-// fs.writeFileSync('./output.txt', content);
+// fs.writeFileSync('./Data/output.txt', content);
 // console.log(content)
 
 //3. fs - read & write file(asynchronous manner):
 // const fs = require('fs');
-// fs.readFile('./start.txt', 'utf-8', (error1, data1) => {
+// fs.readFile('./Data/start.txt', 'utf-8', (error1, data1) => {
 //     console.log(data1);
 //     fs.readFile(`./${data1}.txt`, 'utf-8', (error2, data2) => {
 //         console.log(`${data1}.txt`);
 //         console.log(data2)
 //         console.log(error2);
-//         fs.readFile('./append.txt', 'utf-8', (error3, data3) => {
+//         fs.readFile('./Data/append.txt', 'utf-8', (error3, data3) => {
 //             console.log(data3)
-//             fs.writeFile('./output.txt', `${data2} \n\n${data3}`, () => {
+//             fs.writeFile('./Data/output.txt', `${data2} \n\n${data3}`, () => {
 //                 console.log('Go check your file');
 //             })
 //         })
@@ -64,21 +64,8 @@ const data = JSON.parse(fs.readFileSync('./Data/products.json', 'utf-8'));
 const prodlist = fs.readFileSync('./Template/product-list.html', 'utf-8');
 const prodDetail = fs.readFileSync('./Template/prod-details.html', 'utf-8');
 const url = require('url');
-// console.log(data);
-
-function replaceHtml(template, product){
-    let output = template.replace("{{%NAME%}}", product.name);
-    output = output.replace("{{%MODELNAME%}}", product.modeName);
-    output = output.replace("{{%MODELNO%}}", product.modelNumber);
-    output = output.replace("{{%AGE%}}", product.age);
-    output = output.replace("{{%BEHAVIOUR%}}", product.behaviour);
-    output = output.replace("{{%DESCRIPTION%}}", product.Description);
-    output = output.replace("{{%PRICE%}}", product.price);
-    output = output.replace("{{%COLOR%}}", product.color);
-    output = output.replace("{{%PRODUCTIMAGE%}}", product.productImage);
-    output = output.replace("{{%ID%}}", product.id);
-    return output;
-}
+let replaceHtml = require('./Modules/replaceHtml');
+let contactsData = fs.readFileSync('./Template/contacts-data.html', 'utf-8');
 
 let server = http.createServer((req, res) => {
     //object destructuring:
@@ -103,7 +90,8 @@ let server = http.createServer((req, res) => {
             'Content-Type': 'text/html',
             'my-header': 'meow'
         });
-        res.end(html.replace('{{%CONTENT%}}', 'You are in Meow Contacts page.'));
+        res.end(contactsData);
+        // res.end(html.replace('{{%CONTENT%}}', 'You are in Meow Contacts page.'));
     }
     else if (path.toLocaleLowerCase() === '/products'){
         if(!query.id){
